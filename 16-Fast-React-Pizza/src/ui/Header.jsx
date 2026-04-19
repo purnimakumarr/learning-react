06be02451ae82e9e651723da7d43720f9ff9f45c
Purnima Kumar <purnimak2001@gmail.com>
2026-04-19 14:41:37 +0530

    add project fast-react-pizza

---
+++ 16-Fast-React-Pizza/src/ui/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+
+function Header() {
+  return (
+    <div>
+      <Link to='/'>Fast React Co.</Link>
+
+      <SearchOrder />
+    </div>
+  );
+}
+
+export default Header;
