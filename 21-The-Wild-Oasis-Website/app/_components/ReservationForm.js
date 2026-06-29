0ab05fceea36368b8ee1b56966eba46b84793d38
Purnima Kumar <purnimak2001@gmail.com>
2026-06-29 22:22:55 +0530

    feat(the-wild-oasis): implement authentication with next-auth

--- 21-The-Wild-Oasis-Website/app/_components/ReservationForm.js
+++ 21-The-Wild-Oasis-Website/app/_components/ReservationForm.js
@@ -2,16 +2,16 @@
 
 import { useReservation } from './ReservationContext';
 
-function ReservationForm({ cabin }) {
+function ReservationForm({ cabin, user }) {
   const { maxCapacity } = cabin;
   const { range } = useReservation();
 
   return (
     <div className='scale-[1.01]'>
-      <div className='bg-primary-800 text-primary-300 px-6 py-2 flex justify-between items-center'>
+      <div className='bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center'>
         <p>Logged in as</p>
 
-        {/* <div className='flex gap-4 items-center'>
+        <div className='flex gap-4 items-center'>
           <img
             // Important to display google profile images
             referrerPolicy='no-referrer'
@@ -20,14 +20,10 @@ function ReservationForm({ cabin }) {
             alt={user.name}
           />
           <p>{user.name}</p>
-        </div> */}
+        </div>
       </div>
 
-      <p>
-        {String(range.from)} to {String(range.to)}
-      </p>
-
-      <form className='bg-primary-900 py-10 px-6 text-lg flex gap-5 flex-col'>
+      <form className='bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col'>
         <div className='space-y-2'>
           <label htmlFor='numGuests'>How many guests?</label>
           <select
--- 21-The-Wild-Oasis-Website/app/account/page.js
+++ 21-The-Wild-Oasis-Website/app/account/page.js
@@ -1,11 +1,16 @@
+import { auth } from '../_lib/auth';
+
 export const metadata = {
   title: 'Guest Area',
 };
 
-function Page() {
+async function Page() {
+  const session = await auth();
+  const firstName = session?.user?.name.split(' ').at(0);
+
   return (
     <h2 className='font-semibold text-2xl text-accent-400 mb-7'>
-      Welcome, Purnima
+      Welcome, {firstName}
     </h2>
   );
 }
