03303d093004762a53f6fbe727e158613707cd65
Purnima Kumar <purnimak2001@gmail.com>
2026-07-05 19:42:44 +0530

    minor update

--- 21-The-Wild-Oasis-Website/middleware.js
+++ 21-The-Wild-Oasis-Website/middleware.js
@@ -1,6 +1,7 @@
-import { auth } from '@/app/_lib/auth';
+import NextAuth from 'next-auth';
+import authConfig from '@/app/_lib/auth.config';
 
-export const middleware = auth;
+export const { auth: middleware } = NextAuth(authConfig);
 
 export const config = {
   matcher: ['/account'],
