6f3f65c3593e983fd7009765274ce4a6826b5e32
Purnima Kumar <purnimak2001@gmail.com>
2026-07-05 19:36:48 +0530

    feat(auth): refactor authentication configuration and separate provider setup

---
+++ 21-The-Wild-Oasis-Website/app/_lib/auth.config.js
@@ -0,0 +1,20 @@
+import Google from 'next-auth/providers/google';
+
+export default {
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+
+  callbacks: {
+    authorized({ auth }) {
+      return !!auth?.user;
+    },
+  },
+
+  pages: {
+    signIn: '/login',
+  },
+};
